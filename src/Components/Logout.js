7a865efa278426bb191d2ba2daf0491be9513a99
unknown <godwinprofessional@gmail.com>
2021-03-26 19:49:23 +0100

    first commit

---
+++ src/Components/Logout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { logout } from "../features/userSlice";
+import "./Logout.css";
+
+const Logout = () => {
+  
+  const dispatch = useDispatch();
+  const handleLogout = (e) => {
+    e.preventDefault();
+
+    dispatch(logout());
+  };
+
+  return (
+    <div className="logout">
+      <h1>
+      Welcome <span className="user__name">Godwin</span>
+      </h1>
+       <button className="logout__button" onClick={(e) => handleLogout(e)}>Logout</button>
+    </div>
+  )
+}
+
+export default Logout; 
